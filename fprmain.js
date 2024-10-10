6fc190cb29f079232b0e516d149bfcdae406374e
passAi0 <danielogonnaya.paul91@gmail.com>
2024-10-10 20:30:22 +0100

    Add files via upload

---
+++ fprmain.js
@@ -0,0 +1,4 @@
+// /public/fprmain.js
+(function (w) { w.fpr = w.fpr || function () { w.fpr.q = w.fpr.q || []; w.fpr.q[arguments[0] == 'set' ? 'unshift' : 'push'](arguments); }; })(window);
+fpr("init", { cid: "4zxwtf8m" });
+fpr("click");
